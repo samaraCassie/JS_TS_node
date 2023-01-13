37764d1de9aa9b5787780a52aaab19987279a769
samaraCassie <samaracassie@outlook.com>
2023-01-13 16:57:45 -0300

    Function getNivelImc

--- aula26/assets/js/app.js
+++ aula26/assets/js/app.js
@@ -26,5 +26,21 @@ function meuEscopo() {
     function getImg(peso, altura) {        
         return (peso / altura ** 2).toFixed(2) ;
     }
+
+    function getNivelImc(imc) {
+        if (imc>39.9) {
+            return `Obecidade III`;
+        } else if (imc>=34.9) {
+            return `Obecidade II`;
+        } else if (imc>=29.9) {
+            return `Obecidade I`;
+        }  else if (imc>=24.9) {
+            return `levemente Acima do Peso`;
+        } else if (imc>=18.5) {
+            return `Peso Ideal`;
+        } else{
+            return `Abaixo do Peso`;
+        }
+    }
 }
 meuEscopo();
