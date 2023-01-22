49cec82537e90f9c490427f29711d89d20f9f492
samaraCassie <samaracassie@outlook.com>
2023-01-22 16:19:11 -0300

    Create main.js

---
+++ aula111_XMLHttpRequest/assets/js/main.js
@@ -0,0 +1,43 @@
+const request = obj => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(cht.statusText);
+      }
+    });
+  });
+};
+
+document.addEventListener('click', e => {
+  const element = e.target;
+  const tag = element.tagName.toLowerCase();
+
+  if (tag === 'a') {
+    e.preventDefault();
+    loadPag(element);
+
+  }
+});
+
+function loadPag(el) {
+  const href = el.getAttribute('href');
+  const objConfig = {
+    method: 'GET',
+    url: href
+  };
+
+  request(objConfig).then(response => {
+    loadResult(response);
+  }).catch(error => console.log(error));
+}
+
+function loadResult(response) {
+  const result = document.querySelector('.result');
+  result.innerHTML = response;
+}
